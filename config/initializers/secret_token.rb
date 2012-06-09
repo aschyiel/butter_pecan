eefa14df81529156a5e98822e39c82b1f922bc70
Ulysses Levy <levy.ulysses@gmail.com>
2012-06-08 21:01:43 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ButterPecan::Application.config.secret_token = 'b13650d9ebae24c770e212f675bd55dbb3b0a22af2a9286dd0d9aa07294b43ef405c323d06d4e403422b51f5bfe6c9e838926fefd2dc95df08104cf9088ec6cc'
